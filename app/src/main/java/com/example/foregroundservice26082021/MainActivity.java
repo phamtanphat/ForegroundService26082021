504b957f03d76d899ab2a2ec9dbdf637c8f11eb5
phamtanphat <phatdroid94@gmail.com>
2021-11-02 20:31:05 +0700

    print log

--- app/src/main/java/com/example/foregroundservice26082021/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice26082021/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.foregroundservice26082021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mMainBinding.buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
             }
         });
     }
