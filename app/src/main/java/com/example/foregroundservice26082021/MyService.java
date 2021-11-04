deba82fef5c9d32e88719b376e4c6f76701cf9c6
phamtanphat <phatdroid94@gmail.com>
2021-11-04 21:32:59 +0700

    demo download file

--- app/src/main/java/com/example/foregroundservice26082021/MyService.java
+++ app/src/main/java/com/example/foregroundservice26082021/MyService.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
 import java.util.Random;
@@ -23,6 +24,8 @@ import java.util.Random;
 public class MyService extends Service {
 
     NotificationManager notificationManager;
+    MyDownLoadThread myDownLoadThread;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -34,14 +37,36 @@ public class MyService extends Service {
         super.onCreate();
         Log.d("BBB", "onCreate");
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-
-        startForeground(1,createNotification(String.valueOf(new Random().nextInt())));
+        startForeground(1, createNotification("Loading", 10, 0));
+        myDownLoadThread = new MyDownLoadThread("Thread-1");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        if (!myDownLoadThread.isAlive()) {
+            myDownLoadThread.start();
+            myDownLoadThread.setOnListenerMessage(new OnListenerMessage() {
+                @Override
+                public void onHandleMessage(Message message) {
+                    switch (message.what) {
+                        case 1:
+                            Log.d("BBB", message.obj.toString());
+                            notificationManager.notify(1, createNotification("Loading", 10, (Integer) message.obj));
+                            break;
+                        case 2:
+                            Toast.makeText(getApplicationContext(), "Down load finish", Toast.LENGTH_SHORT).show();
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    stopForeground(true);
+                                    stopSelf();
+                                }
+                            }, 2000);
+                    }
 
+                }
+            });
+        }
         return START_REDELIVER_INTENT;
     }
 
@@ -52,12 +77,14 @@ public class MyService extends Service {
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
 
-    public Notification createNotification(String message){
+    public Notification createNotification(String message, int max, int min) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_SERVICE");
         builder.setSmallIcon(android.R.drawable.ic_dialog_email);
         builder.setShowWhen(true);
-        builder.setContentTitle("Tin nhắn");
+        builder.setContentTitle("Down load");
         builder.setContentText(message);
+        builder.setProgress(max, min, false);
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel("CHANNEL_SERVICE", "CHANNEL", NotificationManager.IMPORTANCE_DEFAULT);
@@ -65,6 +92,51 @@ public class MyService extends Service {
         }
 
         return builder.build();
+    }
+
+    class MyDownLoadThread extends HandlerThread {
+        Handler handler;
+        OnListenerMessage onListenerMessage;
+
+        public MyDownLoadThread(String name) {
+            super(name);
+        }
+
+        @Override
+        protected void onLooperPrepared() {
+            super.onLooperPrepared();
+            handler = new Handler(getLooper()) {
+                @Override
+                public void handleMessage(@NonNull Message msg) {
+                    super.handleMessage(msg);
+                    onListenerMessage.onHandleMessage(msg);
+                }
+            };
+            for (int i = 1; i <= 10; i++) {
+                Message message = new Message();
+                message.what = 1;
+                message.obj = i;
+                handler.dispatchMessage(message);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Message message2 = new Message();
+            message2.what = 2;
+            handler.sendMessage(message2);
+
+
+        }
+
+        public void setOnListenerMessage(OnListenerMessage onListenerMessage) {
+            this.onListenerMessage = onListenerMessage;
+        }
+    }
+
+    interface OnListenerMessage {
+        void onHandleMessage(Message message);
     }
 
 }
